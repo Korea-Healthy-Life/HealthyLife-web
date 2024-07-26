b471ec1942051c64151ad01df806e681640bda4f
als5855 <wlals5855@naver.com>
2024-07-26 10:02:16 +0900

    20240726-01

--- healthy-life/src/views/home/search/SearchList.tsx
+++ healthy-life/src/views/home/search/SearchList.tsx
@@ -1,38 +1,45 @@
-import React from 'react'
-import { Searchs } from './SearchApp'
-import '../../../style/home/Search.css'
+import React from "react";
+import "../../../style/home/SearchList.css";
+import Search from "./Search";
+import { Searchs } from "./SearchApp";
 
 interface SearchPops {
   searchs: Searchs[];
-  onToggle: (id: number) => void;
-  onRemove: (id: number) => void;
+  toggleSearch: (id: number) => void;
+  deleteSearch: (id: number) => void;
 }
 
-export default function SearchList({searchs, onToggle, onRemove}: SearchPops) {
+export default function SearchList({
+  searchs,
+  toggleSearch,
+  deleteSearch,
+}: SearchPops) {
   return (
-    <div className='searchContainer'>
-      <div className='currentSearch'>
+    <div className="searchContainer">
+      <div className="currentSearch">
         <h5>최근검색어</h5>
-      {searchs.map((search) => (
-        <div key={search.id}>
-          <ul>
-            <li onClick={() => onToggle(search.id)}>{search.title}</li>
-          </ul>
-          <button onClick={()=> onRemove(search.id)}>x</button>
-        </div>
-      ))}
+        <ul>
+          <li>
+            {searchs.map((search) => (
+              <Search
+                key={search.id}
+                search={search}
+                toggleSearch={toggleSearch}
+                deleteSearch={deleteSearch}
+              />
+            ))}
+          </li>
+        </ul>
       </div>
 
-      <div className='line'></div>
+      <div className="line"></div>
 
-        <div className='popularSearch'>
-          <h5>인기키워드</h5>
-          <ul>
-            <li></li>
-          </ul>
-        </div>
-
+      <div className="popularSearch">
+        <h5>인기키워드</h5>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
     </div>
-  
-  )
+  );
 }
