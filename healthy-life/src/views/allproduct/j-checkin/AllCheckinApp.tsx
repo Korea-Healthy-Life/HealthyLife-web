7eafa3abea328a4ac4e6aac97f7feacd6d669ac6
als5855 <wlals5855@naver.com>
2024-07-26 21:32:43 +0900

    20240726-new

--- healthy-life/src/views/allproduct/j-checkin/AllCheckinApp.tsx
+++ healthy-life/src/views/allproduct/j-checkin/AllCheckinApp.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function AllCheckinApp() {
   return (
-    <div>AllCheckinApp</div>
+    <div>닭가슴살/육류
+      
+    <ul>
+      <li><button>닭가슴살</button></li>
+      <li><button>달걀</button></li>
+      <li><button>소고기</button></li>
+    </ul>
+
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+    </div>
   )
 }
