d4e229111687b831f3f8eb07ee26efd707619cf2
als5855 <wlals5855@naver.com>
2024-08-21 21:51:51 +0900

    20240821-jjm-new

--- healthy-life/src/views/mypage/mypage-main/Mypage.tsx
+++ healthy-life/src/views/mypage/mypage-main/Mypage.tsx
@@ -1,59 +1,61 @@
-import React, { useState } from 'react'
-import '../../../style/mypage/MypageMain.css'
-import PercentSharpIcon from '@mui/icons-material/PercentSharp';
-import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
-import CardMembershipIcon from '@mui/icons-material/CardMembership';
-import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import React, { useState } from "react";
+import "../../../style/mypage/MypageMain.css";
 
-
-
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 
 function Mypage() {
   const [currentPage, setCurrentPage] = useState<number>(0);
-  const [productPerpage] = useState<number>(16)
+  const [productPerpage] = useState<number>(16);
 
   return (
     <div>
-    <div className='myPageConatiner'>
-    <div className='mypageAllContainer'>
-      <div className='mypageMainContainer'>
-        <h2>마이 쇼핑</h2>
-        <ul>
-          <li><AccountCircleIcon /><br />회원이름</li>
-          <li><MilitaryTechIcon /><br />회원등급</li>
-          <li><CardMembershipIcon /><br />멤버십</li>
-          <li><MonetizationOnIcon /><br /><span>0원</span><br />적립금</li>
-          <li><PercentSharpIcon /><br /><span>0개</span><br />쿠폰 </li>
-          <li><LocalShippingIcon /><br /><span>0개</span><br />주문</li>
-        </ul>
-      </div>
-
-      <div className='mypageSuvContainer'>
-        <h4>나의 주문처리현황</h4>
-        <ul>
-          <li>0 <br /> 결제</li>
-          <li>&#62;</li>
-          <li>0 <br /> 배송준비중
-          </li>
-          <li>&#62;</li>
-          <li>0 <br /> 배송중</li>
-          <li>&#62;</li>
-          <li>0 <br /> 배송완료
-          </li>
-        </ul>
+      <div className="myPageConatiner">
+        <div className="mypageAllContainer">
+          <div className="mypageSuvContainer">
+            <h4>나의 주문처리현황</h4>
+            <ul className="olderListUlA">
+              <li>
+                {" "}
+                <span className="mypagePrice">0</span>
+                <span>결제</span>
+              </li>
+              <li className="orderArrow">
+                <span className="orderArrow">
+                  <ArrowForwardIosRoundedIcon />
+                </span>
+              </li>
+              <li>
+                <span className="mypagePrice">0</span>
+                <span>배송준비중</span>
+              </li>
+              <li className="orderArrow">
+                <span className="orderArrow">
+                  <ArrowForwardIosRoundedIcon />
+                </span>
+              </li>
+              <li>
+                <span className="mypagePrice">0</span> <span>배송중</span>
+              </li>
+              <li>
+                <span className="orderArrow">
+                  <ArrowForwardIosRoundedIcon />
+                </span>
+              </li>
+              <li>
+                <span className="mypagePrice">0</span> <span>배송완료</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="orderListCotainer">
+          <h4>주문내역</h4>
+          <ul className="orderListUl">
+            <li>주문내역</li>
+          </ul>
+        </div>
       </div>
     </div>
-    <div className='orderListCotainer'>
-      <h4>주문내역</h4>
-      <ul className='orderListUl'>
-        <li>주문내역</li>
-      </ul>
-    </div> 
-    </div>
-  </div>
-  )
+  );
 }
 
-export default Mypage
+export default Mypage;
--- healthy-life/src/views/mypage/mypage-main/MypageNav.tsx
+++ healthy-life/src/views/mypage/mypage-main/MypageNav.tsx
@@ -10,15 +10,22 @@ const MypageNav:React.FC = () => {
   
   return (
     <div className='mypagNavContainer'>
-      <div className='myPageNavBox'>
-        <ul>
-        <li><Link to='/mypage/userinformation'>내 정보</Link></li> 
+      <div className='myPageNavDiv'>
+        <ul className='myPageNavBox'>
+        <li className='mypageNavTitle'>주문관리</li>
         <li><Link to='/mypage/orderApp'>주문처리 현황</Link></li> 
+        <li className='mypageNavLine'></li>
+        <li className='mypageNavTitle'>활동관리</li>
         <li><Link to='/mypage/myreview'>후기</Link></li> 
         <li><Link to='/mypage/wishlist'>위시리스트</Link></li> 
         <li><Link to='/mypage/current'>최근 상품목록</Link></li> 
+        <li className='mypageNavLine'></li>
+        <li className='mypageNavTitle'>혜택관리</li>
         <li><Link to='/mypage/mileage'>적립금 내역</Link></li> 
         <li><Link to='/mypage/membership'>등급별 혜택</Link></li> 
+        <li className='mypageNavLine'></li>
+        <li className='mypageNavTitle'>회원정보관리</li>
+        <li><Link to='/mypage/userinformation'>내 정보</Link></li> 
         </ul>
       </div>
     </div>
